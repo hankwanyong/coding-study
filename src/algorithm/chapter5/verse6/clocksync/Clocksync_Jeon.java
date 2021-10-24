d7724a653537c791b916b39ec2f677f6c0f94c5d
Caf <Caf@DESKTOP-O8P5QFM>
2021-10-24 13:52:46 +0900

    algo

---
+++ src/algorithm/chapter5/verse6/clocksync/Clocksync_Jeon.java
@@ -0,0 +1,15 @@
+package algorithm.chapter5.verse6.clocksync;
+
+public class Clocksync_Jeon {
+	/*
+	 * 시계 맞추기 page.168
+	 */
+	
+	
+	public static void main(String[] args) {
+	
+		
+	}
+	
+	
+}
--- src/effectivejava/chapter5/item33/Item33Main.java
+++ src/effectivejava/chapter5/item33/Item33Main.java
@@ -1,13 +1,8 @@
 package effectivejava.chapter5.item33;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Item33Main {
 	/*
